5ce11792a2231b2bbc2cbdadeeb31bb72a2ad549
mohon63 <mohonhossen63@gmail.com>
2021-09-22 10:08:17 +0600

    using props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Device from './components/Device/Device';
 
 function App() {
   return (
     <div className="App">
-      <p>npm = Node Package Manager</p>
-      <p>cli = Command Line Interface</p>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Device name="Phone" price="13000" />
+
     </div>
   );
 }
